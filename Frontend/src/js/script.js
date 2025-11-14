5f6a24a6c2d82e00a683f1f9d1a3bb7c98da2249
Lucas Silva <lucascarlosjobs@gmail.com>
2025-11-13 23:58:53 -0300

    feat: implementa dados do backend no frontend

--- Frontend/src/js/script.js
+++ Frontend/src/js/script.js
@@ -14,24 +14,51 @@ async function submit(id) {
       console.warn("Nenhuma linha encontrada para o ID:", id);
       return;
     }
-    const codigoLinha = resultado[3].cl; 
-    const URL_HORARIO = `${PROXY_BASE}/Previsao/Linha?codigoLinha=${codigoLinha}`;
-
-    const resultadoHorario = await $.ajax({
-      url: URL_HORARIO,
-      method: "GET",
+    $("#tabela-corpo").empty();
+    const tablebody = $("#tabela-corpo");
+    resultado.forEach(async function (linha, i) {
+      const horario = await pegaHorario(resultado[i].cl);
+      console.log("Horário da linha", resultado[i].cl, ":", horario);
+      const dadoTabela = `
+      <tr>
+        <td>${linha.cl}</td>
+        <td>${linha.lc}</td>
+        <td>${linha.lt}</td>
+        <td>${linha.sl}</td>
+        <td>${linha.tl}</td>
+        <td>${linha.tp}</td>
+        <td>${linha.ts}</td>
+        <td>${horario.hr}</td>
+      </tr>
+      `;
+      tablebody.append(dadoTabela);
     });
 
-    console.log("Resultados obtidos:", { linha: resultado[3], horario: resultadoHorario });
-    return resultadoHorario;
+    console.log("Linha encontrada:", resultado);
 
+    console.log("Resultados obtidos:", {
+      linha: resultado[3],
+      horario: resultadoHorario,
+    });
+    return resultadoHorario;
   } catch (error) {
     if (error.status === 401) {
-      console.error("ERRO 401: Falha na autenticação. Verifique seu token de API.", error);
+      console.error(
+        "ERRO 401: Falha na autenticação. Verifique seu token de API.",
+        error
+      );
     } else {
       console.error("ERRO na requisição AJAX:", error);
     }
   }
+}
+
+async function pegaHorario(codigoLinha) {
+  const URL_HORARIO = `${PROXY_BASE}/Previsao/Linha?codigoLinha=${codigoLinha}`;
+  return await $.ajax({
+    url: URL_HORARIO,
+    method: "GET",
+  });
 }
 
 form.addEventListener("submit", function (event) {
